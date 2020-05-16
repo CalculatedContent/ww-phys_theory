dad3121e7200e7a3458eb8a5abea0e6f55a6d186
John Shin <johnyoungshin@gmail.com>
2020-05-16 12:53:56 -0400

    "working on standalone Jacobian py file"

--- Jacobian/jacobian.py
+++ Jacobian/jacobian.py
@@ -1,4 +1,6 @@
 """
-Helper Functions to compute J^T J and J J^T, 
-were J is the Jacobian of the Neural Network
+Helper Functions to compute the spectrum of J^T*J and J*J^T, 
+were J is the Jacobian of the Neural Network J = df(x)/dx.
+the diagonal J will be of dimension training_data_size*(output_dim*data_dim)
+while the Full J will be of dimension training_data_size*(output_dim*training_data_size*data_dim)
 """
