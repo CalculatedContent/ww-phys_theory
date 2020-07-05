1e24d9ed504ad7feaa2868c883a8a960b248083c
John Shin <johnyoungshin@gmail.com>
2020-07-05 16:44:54 -0400

    skeleton for random jacobian function

--- Jacobian/model_jacobian.py
+++ Jacobian/model_jacobian.py
@@ -5,3 +5,9 @@ Functions to generate correlated random matrices and compute their spectra.
 import torch
 import numpy as np
 import math
+
+def random_jacobian(n, d, classes=10, correlation=1.0):
+	"""Generates a random jacobian of dimension (n, d)
+	"""
+	pass
+
